5e3d52512a8612672f1ec8c2fedaa27cc17fb50b
Giovani L. Rozza <giovanni.rozza@gmail.com>
2022-10-07 00:59:20 -0300

    added GSTConsoleDialog class.

---
+++ src/view/GSTConsoleDialog.java
@@ -0,0 +1,30 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Window;
+
+import javax.swing.JDialog;
+
+import utilities.GSTLabels;
+
+public class GSTConsoleDialog extends JDialog implements GSTLabels {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8208899131056172974L;
+
+	
+	//public ConsoleGSTFrame(Window parent, ParametrosGST parGST) {
+	public GSTConsoleDialog(Window parent, boolean bModal) {
+		super(parent);	
+ 		this.setModal(bModal);
+		this.setTitle("Sum�rio dos C�lculos da S�rie Temporal");	
+		this.setPreferredSize(new Dimension(660,400)); 		
+		GSTTelaMenu.SetaPosicaoJanela(this,new Dimension(getPreferredSize()));
+		this.setResizable(false);    
+		this.getContentPane().add(new GSTConsolePanel());  
+ 		
+ 	}
+
+}
